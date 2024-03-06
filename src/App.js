5c8787eab47e71aae431193e02becbe91ad4cfc2
Skish03 <sak00009@mix.wvu.edu>
2024-03-05 19:52:36 -0500

    week 8 lab

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
 import './App.css';
-import './navbar.js';
-import './card.js';
+import Navbar from './navbar.js'; 
+import Card from './card.js';
 
 function App() {
     return (
         <div className="App">
-            <nav class="navbar navbar-light bg-light">
-                <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </nav>
+
+            < Navbar />
 
             <h1><strong> CS 230 Lab </strong></h1>
             <h2><strong> Section - 001 </strong></h2>
             <p> WVU ID: 800358898 </p>
             <p> Hi I am Stephanie. </p>
+            
+            < Card />
 
-            <div class="card">
-                <h5 class="card-title">Card 01</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-
-            <div class="card">
-                <h5 class="card-title">Card 02</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-
-            <div class="card">
-                <h5 class="card-title">Card 03</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
         </div>
     );
 }
-
 export default App;
--- src/card.js
+++ src/card.js
@@ -1,8 +1,30 @@
-<div class="card" style="width: 18rem;">
-    <img class="card-img-top" src="..." alt="Card image cap"> </img>
-    <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
-    </div>
-</div>
+import React from 'react';
+
+function Card() {
+    return (
+        <div class="card-group">
+            <div class="card bg-primary text-white" >
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+            </div>
+            <div class="card bg-warning text-white">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+            </div>
+            <div class="card bg-success text-white">
+                    <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    </div>
+            </div>
+        </div>
+    );
+}
+export default Card;
